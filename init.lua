194d3f9e599381d2ae80a74773a909cb8f858a6e
AssachanDev <assachan.senpaseuth@gmail.com>
2026-02-02 11:36:14 +0700

    add/ plugins: debug,fidget,format,lualine and modified/ lsp.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,16 @@
--- Load basic options
+-- [[ แก้บั๊ก Telescope สำหรับ Neovim 0.10+ ]]
+local ts = vim.treesitter
+if ts.language and not ts.language.ft_to_lang then
+    ts.language.ft_to_lang = function(ft)
+        local lang = ts.get_lang(ft)
+        return lang or ft
+    end
+end
+
+-- [[ ตั้งค่าความเร็วระบบ ]]
+vim.opt.updatetime = 300 -- ทำให้หน้าต่าง Error เด้งไวขึ้น (0.3 วินาที)
+
+-- [[ เรียกใช้ Config อื่นๆ ]]
 require("config.options")
-
--- Setup lazy.nvim and load plugins
-require("config.lazy")
-
--- Load keymaps
 require("config.keymaps")
+require("config.lazy")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,7 @@
 return {
   {
     "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
+    config = function() require("mason").setup() end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
@@ -16,17 +14,20 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      
-      -- Python
-      if vim.fn.executable("basedpyright") == 1 then
-        vim.lsp.enable("basedpyright")
-      end
+      -- Python & Lua
+      if vim.fn.executable("basedpyright") == 1 then vim.lsp.enable("basedpyright") end
+      if vim.fn.executable("lua-language-server") == 1 then vim.lsp.enable("lua_ls") end
 
-      -- Lua
-      if vim.fn.executable("lua-language-server") == 1 then
-        vim.lsp.enable("lua_ls")
-      end
+      -- Diagnostic Settings
+      vim.diagnostic.config({
+        virtual_text = true,
+        float = { border = "rounded", source = "always" },
+      })
 
+      -- หน้าต่าง Error เด้งไว (ต้องไปแก้ updatetime ใน init.lua หรือ options.lua ด้วยนะ)
+      vim.api.nvim_create_autocmd("CursorHold", {
+        callback = function() vim.diagnostic.open_float(nil, { focusable = false }) end,
+      })
     end,
   },
   {
@@ -34,28 +35,39 @@ return {
     dependencies = { 
       "hrsh7th/cmp-nvim-lsp", 
       "L3MON4D3/LuaSnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
     },
     config = function()
       local cmp = require("cmp")
       cmp.setup({
         snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+          expand = function(args) require("luasnip").lsp_expand(args.body) end,
+        },
+        -- เพิ่มส่วน Formatting เพื่อให้เห็น [Buffer] [LSP] [Path]
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              buffer   = "[Buffer]",
+              path     = "[Path]",
+            })[entry.source.name]
+            return vim_item
           end,
         },
         mapping = cmp.mapping.preset.insert({
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
+            if cmp.visible() then cmp.select_next_item() else fallback() end
           end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "buffer", keyword_length = 3 },
+          { name = "path" },
         }),
       })
     end,
