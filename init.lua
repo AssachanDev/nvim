2f282540cc6a0adfe5d0c328cfff319319c871fb
AssachanDev <assachan.senpaseuth@gmail.com>
2026-02-13 20:59:15 +0700

    modified/ plugins: lsp.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- [[ แก้บั๊ก Telescope สำหรับ Neovim 0.10+ ]]
 local ts = vim.treesitter
 if ts.language and not ts.language.ft_to_lang then
 	ts.language.ft_to_lang = function(ft)
@@ -7,10 +6,8 @@ if ts.language and not ts.language.ft_to_lang then
 	end
 end
 
--- [[ ตั้งค่าความเร็วระบบ ]]
-vim.opt.updatetime = 3000 -- ทำให้หน้าต่าง Error เด้งไวขึ้น (0.3 วินาที)
+vim.opt.updatetime = 3000
 
--- [[ เรียกใช้ Config อื่นๆ ]]
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
